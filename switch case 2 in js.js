62f5266d0f68c9f633dacfa932e79fb409249809
KariukiAbel <abelmuriithi@gmail.com>
2020-02-25 03:30:00 +0300

    second switch case practice in js

---
+++ switch case 2 in js.js
@@ -0,0 +1,23 @@
+function switchOfStuff(val) {
+    var answer = "";
+    // Only change code below this line
+    switch(val){
+        case "a":
+            answer = "apple"
+            break
+        case "b":
+            answer = "bird"
+            break
+        case "c":
+            answer = "cat"
+            break
+        default :
+            answer = "stuff"
+    }
+
+    return answer;
+}
+
+console.log(switchOfStuff(1));
+console.log(switchOfStuff("a"));
+
