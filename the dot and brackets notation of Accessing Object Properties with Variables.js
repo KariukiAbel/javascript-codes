edfc41e70d6f2d29ee29648db70ef7ce1a45b2e5
KariukiAbel <abelmuriithi@gmail.com>
2020-02-26 21:12:26 +0300

    Learnt the dot and brackets notations to access properties in an object

---
+++ the dot and brackets notation of Accessing Object Properties with Variables.js
@@ -0,0 +1,18 @@
+// Setup
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water",
+    "car": "Ford Mustang",
+    "house": "bungalo"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj['an entree'];   // Change this line
+var drinkValue = testObj['the drink'];    // Change this line
+console.log(entreeValue)
+console.log(drinkValue)
+console.log(testObj["my side"])
+console.log(testObj.car)
+console.log(testObj.house)
